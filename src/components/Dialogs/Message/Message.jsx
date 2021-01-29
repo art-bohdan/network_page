164f747531ebb0175ce32c8bd54d79a407519435
Bohdan <bohdanprog@gmail.com>
2021-01-29 11:58:45 +0100

    fixed project

--- src/components/Dialogs/Message/Message.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import classes from './../Dialogs.module.css';
-
-
-const Message = props => {
-  return <div className={classes.massage}>{props.message}</div>;
-};
-
-export default Message;
